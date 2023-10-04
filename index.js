b1b29da271fc211278d5503b00ffc56e1850489d
cristian07rl <cristian07rl@hotmail.com>
2023-10-04 02:08:12 -0500

    "message"

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const fs = require("fs")
 const app = express();
 // Habilita CORS para todas las rutas
 app.use(cors());
-const port = 3000; // Puedes cambiar el puerto según tus preferencias
+const PORT = process.env.PORT ?? 3000; // Puedes cambiar el puerto según tus preferencias
 
 var temperature, humidity, voltage, consumption, Control;
 // Datos simulados de monitoreo de una planta solar
@@ -13,6 +14,22 @@ let solarData = {};
 // Configura el middleware body-parser para analizar las solicitudes POST
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//ruta main para desplegar el html
+app.get('/', (req, res) => {
+  // Lee el archivo HTML desde la misma carpeta que el script
+  fs.readFile('dashboar.html', 'utf8', (err, data) => {
+    if (err) {
+      // Maneja errores si no se puede leer el archivo
+      console.error(err);
+      res.status(500).send('Error interno del servidor');
+    } else {
+      // Configura la cabecera de la respuesta para indicar que se está enviando HTML
+      res.setHeader('Content-Type', 'text/html');
+      // Envía el contenido del archivo HTML como respuesta
+      res.send(data);
+    }
+  });
+})
 
 // Ruta para obtener los datos de monitoreo
 app.get('/monitoreo', (req, res) => {
@@ -43,6 +60,6 @@ app.post('/guardar_datos', (req, res) => {
 });
 
 
-app.listen(port, () => {
-    console.log(`La API está escuchando en el puerto ${port}`);
+app.listen(PORT, () => {
+    console.log(`La API está escuchando en el puerto ${PORT}`);
 });
