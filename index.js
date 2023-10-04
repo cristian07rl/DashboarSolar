34b2acd07fe6d90056b6178842fb7173a89c0be2
cristian07rl <cristian07rl@hotmail.com>
2023-10-04 02:28:16 -0500

    little fix

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const app = express();
 // Habilita CORS para todas las rutas
 app.use(cors());
 const PORT = process.env.PORT ?? 3000; // Puedes cambiar el puerto según tus preferencias
-
-var temperature, humidity, voltage, consumption, Control;
 // Datos simulados de monitoreo de una planta solar
 let solarData = {};
 
@@ -15,21 +13,6 @@ let solarData = {};
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //ruta main para desplegar el html
-app.get('/', (req, res) => {
-  // Lee el archivo HTML desde la misma carpeta que el script
-  fs.readFile('dashboar.html', 'utf8', (err, data) => {
-    if (err) {
-      // Maneja errores si no se puede leer el archivo
-      console.error(err);
-      res.status(500).send('Error interno del servidor');
-    } else {
-      // Configura la cabecera de la respuesta para indicar que se está enviando HTML
-      res.setHeader('Content-Type', 'text/html');
-      // Envía el contenido del archivo HTML como respuesta
-      res.send(data);
-    }
-  });
-})
 
 // Ruta para obtener los datos de monitoreo
 app.get('/monitoreo', (req, res) => {
