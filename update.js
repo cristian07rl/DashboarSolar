8a8237692e6b338cbf204f8e2ccd8625b77ed569
cristian07rl <cristian07rl@hotmail.com>
2023-12-03 10:17:35 -0500

    Esp8266 code

--- update.js
+++ update.js
@@ -1,6 +1,7 @@
-/* global Chart */
-const chartCanvas = document.getElementById('dataChart')
-const ctx = chartCanvas.getContext('2d')
+import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
+import { tempChart } from './graficas.js'
+const socket = io()
+
 // Arreglos para almacenar los datos y etiquetas de tiempo
 const timeLabels = []
 const temperatureData = []
@@ -8,89 +9,45 @@ const temperatureData = []
 // const voltageData = []
 // const consumptionData = []
 const Datapoints = 6
+socket.on('chat message', (data) => {
+  const humidity = data[Datapoints - 1].humedad
+  const voltage = data[Datapoints - 1].voltaje
+  const consumption = data[Datapoints - 1].consumo
+  const control = data[Datapoints - 1].control
+  if (temperatureData.length === 0) {
+    data.forEach(data => {
+      const temperature = data.temperatura
+      const currentTime = data.hora.toString().slice(11, data.hora.indexOf('.'))
+      temperatureData.push(temperature)
+      timeLabels.push(currentTime)
+    })
+  } else {
+    const temperature = data[Datapoints - 1].temperatura
+    temperatureData.push(temperature)
+    const currentTime = data[Datapoints - 1].hora.toString().slice(11, data[0].hora.indexOf('.'))
+    timeLabels.push(currentTime)
+  }
+  // Puedes guardar los datos en una variable si lo deseas
+  document.getElementById('temperature').textContent = temperatureData[Datapoints - 1] + ' °C'
+  document.getElementById('humidity').textContent = `${humidity} %`
+  document.getElementById('voltage').textContent = `${voltage} V`
+  document.getElementById('consumption').textContent = `${consumption} W`
+  document.getElementById('control').textContent = control
+
+  updateChart() // Actualizar el gráfico
+})
 function updateData () {
   // "https://dashboarsolar-rxzu-dev.fl0.io/monitoreo"
   // "http://localhost:3000/monitoreo"
-  fetch('https://dashboarsolar-rxzu-dev.fl0.io/monitoreo')
-    .then(function (response) {
-      if (!response.ok) {
-        throw new Error('Error en la solicitud: ' + response.status)
-      }
-      return response.json() // Esto convierte la respuesta en un objeto JSON
-    })
-    .then(function (data) {
-      // Aquí puedes acceder a los datos JSON y guardarlos
-      console.log(data)
-      console.log(data[Datapoints - 1].hora)
-      const humidity = data[Datapoints - 1].humedad
-      const voltage = data[Datapoints - 1].voltaje
-      const consumption = data[Datapoints - 1].consumo
-      const control = data[Datapoints - 1].control
-      if (temperatureData.length === 0) {
-        data.forEach(data => {
-          const temperature = data.temperatura
-          const currentTime = data.hora.toString().slice(11, data.hora.indexOf('.'))
-          temperatureData.push(temperature)
-          timeLabels.push(currentTime)
-        })
-      } else {
-        const temperature = data[Datapoints - 1].temperatura
-        temperatureData.push(temperature)
-        const currentTime = data[Datapoints - 1].hora.toString().slice(11, data[0].hora.indexOf('.'))
-        timeLabels.push(currentTime)
-      }
-      // Puedes guardar los datos en una variable si lo deseas
-      document.getElementById('temperature').textContent = temperatureData[Datapoints - 1] + ' °C'
-      document.getElementById('humidity').textContent = `${humidity} %`
-      document.getElementById('voltage').textContent = `${voltage} V`
-      document.getElementById('consumption').textContent = `${consumption} W`
-      document.getElementById('control').textContent = control
-      // Actualizar el gráfico
-      updateChart()
-      // Realiza cualquier otra operación con los datos
-    })
-    .catch(function (error) {
-      console.error(error)
-    })
+  socket.emit('monitoreo', '')
 }
 // Crear un gráfico de línea
-const dataChart = new Chart(
-  ctx, {
-    type: 'line',
-    data: {
-      labels: timeLabels,
-      datasets: [
-        {
-          label: 'Temperatura (°C)',
-          data: temperatureData,
-          fill: false,
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        x: {
-        },
-        y: {
-          beginAtZero: true,
-          title: {
-            display: true,
-            text: 'Temperatura (°C)'
-          }
-        }
-      }
-    }
-  })
 
 // Función para actualizar el gráfico
 function updateChart () {
-  dataChart.data.labels = timeLabels
-  dataChart.data.datasets.data = temperatureData
-  dataChart.update()
+  tempChart.data.labels = timeLabels
+  tempChart.data.datasets[0].data = temperatureData
+  tempChart.update()
   if (timeLabels.length >= Datapoints) {
     timeLabels.shift()
     temperatureData.shift()
